8588eff5d75ecaa5365f405da7404f559878a0ba
imanolmur1770 <imanolmurillo@gmail.com>
2023-09-16 10:47:56 -0400

    Initial commit

---
+++ bookshub-backend/src/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+let articlesinfo = [{
+    name: 'learn-react',
+    upvotes: 0,
+    comments: [],
+}, {
+    name: 'learn-node',
+    upvotes: 0,
+    comments: [],
+}, {
+    name: 'mongodb',
+    upvotes: 0,
+    comments: [],
+}]
+
+const app = express();
+app.use(express.json());
+
+app.put('/api/articles/:name/upvote', (req,res) => {
+    const { name } = req.params;
+    const article = articlesinfo.find(a => a.name === name);
+    if(article){
+        article.upvotes +=1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+    }else{
+        res.send('That article doesnt\'t exist');
+    }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const { postedBy, text } = req.body;
+    const { name } = req.params;
+
+    const article = articlesinfo.find(a => a.name === name);
+
+    if(article){
+        article.comments.push({postedBy, text});
+        res.send(article.comments);
+    }else{
+        res.send('That article doesnt\'t exist');
+    }
+});
+
+app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+});
---
+++ bookshub/src/pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+const NotFoundPage = () => (
+    <h1>404: Page Not FOund!</h1>
+);
+
+export default NotFoundPage;
